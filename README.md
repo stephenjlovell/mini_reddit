9b564f8844958bbae938055f9f4d3fbeca3007db
Steve Lovell <sjlovell34@gmail.com>
2015-03-06 17:52:26 -0500

    setup rspec and devise

---
+++ README.md
@@ -0,0 +1,3 @@
+# mini_reddit
+
+A Reddit clone implemented in Rails 4.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
+  resources :home
+  
+  root to: "home#index"
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +60,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
